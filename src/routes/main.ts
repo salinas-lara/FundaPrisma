bbde69b514a8523f1d83ce22b22550ca46878e44
Lara Salinas <l.salinas.if@gmail.com>
2026-05-11 07:28:54 -0300

    atualizar

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,7 +1,21 @@
 import { Router } from 'express';
+import { prisma } from '../libs/prisma'
+import { createUser } from '../services/user';
 
 export const mainRouter = Router();
 
 mainRouter.get('/ping', (req, res) => {
     res.json({ pong: true });
 });
+
+mainRouter.post('/user', async (req, res) => {
+    const user = await createUser({
+        name: 'John Doe',
+        email: 'john.doe@exemple.com'
+    });
+    if (user) {
+        res.status(201).json({ user });
+    } else {
+        res.status(400).json({ error: 'Email already exists' });
+    }
+})
---
+++ src/services/user.ts
@@ -0,0 +1,27 @@
+import { Prisma } from "@prisma/client"
+import { prisma } from "../libs/prisma"
+
+type CreateUserProps = {
+	name: string
+	email: string
+}
+
+export const createUser = async ({ name, email }: CreateUserProps) => {
+	try {
+		const user = await prisma.user.create({
+			data: {
+				name,
+				email
+			}
+		})
+
+		return user
+	} catch (error) {
+		if (error instanceof Prisma.PrismaClientKnownRequestError) {
+			if (error.code === 'P2002') {
+				console.error('Error: Email already exists')
+				return false
+			}
+		}
+	}
+}
